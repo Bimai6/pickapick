1f30b269e7ba81d34f8b566dc1d19779b2746431
Bimai6 <bimaivfx@gmail.com>
2025-11-05 12:48:40 +0100

    feat: add connection to mongo atlas cluster and cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+import cors from "cors";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Server is running"));
@@ -12,3 +16,8 @@ const port = Number(process.env.PORT ?? 3000);
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
 })
+
+mongoose
+    .connect(String(process.env.MONGO_URI))
+    .then(()=> console.log("Connected to MongoDB Atlas"))
+    .catch((error)=> console.log(error))
