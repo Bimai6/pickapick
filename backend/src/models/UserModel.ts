df83b02405824a7942aa4c4425e5ba93263dd6df
Bimai6 <bimaivfx@gmail.com>
2025-11-18 14:43:56 +0100

    feat: add UserModel and bcryptjs on packages

---
+++ backend/src/models/UserModel.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required:true, trim:true},
+    password: {type: String, required:true, trim:true},
+    email: {type: String, required:true, trim:true},
+    avatar: {type: String, trim:true},
+    favPokemon: {type: String, trim:true}, //Podemos anadir defaults a estos tres ultimos campos, no se los pongo aun porque no tenemos imagenes
+    teamFlag: {type: String, trim:true}
+})
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+    return bcrypt.compare(enteredPassword, this.password);
+}
+
+export default mongoose.model('User', userSchema);
