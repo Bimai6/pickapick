86460e89c854f76044af841dd45a8221924cd155
Bimai6 <bimaivfx@gmail.com>
2025-11-27 13:52:44 +0100

    feat: add new register and login endpoints with middleware verifyToken

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { loginUser } from "../controllers/UserController.js";
+import { registerUser } from "../controllers/UserController.js";
+import { verifyToken } from "../middlewares/auth.js";
 
 const api = express.Router();
 
 api.get("/", (req, res)=> {
     res.send("You are on the API route access");
-} )
+})
+
+api.get("/login", verifyToken, loginUser);
+api.get("/register", verifyToken, loginUser);
 
 export default api;
--- backend/src/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.get('/', async(req, res) => {
-    "You are in the API"
-})
-
-export default router;
