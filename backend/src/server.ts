82e346bd438f96be611f478a6328ae01620a129b
Bimai6 <bimaivfx@gmail.com>
2025-11-27 15:26:44 +0100

    feat: add corsOptions to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,12 +5,13 @@ import cors from "cors";
 import { buildPokemonList } from "./services/pokeapiService.js";
 import api from "./routes/api.js";
 import pokemonRouter from "./routes/pokemonRoutes.js";
+import { corsOptions } from "./config/corsOptions.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", api);
 app.use("/pokemon", pokemonRouter)
